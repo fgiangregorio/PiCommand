e65f6b355f33ac7e6304bd74ea10aaa62b02e71e
fgiangregorio <giaf@global.co.za>
2016-10-02 08:17:53 +0300

    Work done by De la Rey with some initial eye expressions

--- DroidEye/DroidEye.X/main.c
+++ DroidEye/DroidEye.X/main.c
@@ -1,4 +1,3 @@
-
 // Device: PIC16LF1829
 // Using xc8 v1.30
 //
@@ -8,11 +7,12 @@
 //
 //  1.00aA  FG, 19 Aug 15, created
 //  1.00aB  FG, 11 Jul 16, started adding support for UART RX
+//  1.00aC  DC, 15 Jul 16, added several expressions and mirroring
+//  1.00aD  DC, 22 Jul 16, added additional expressions, serial commands implemented but untested
 //
 //***********************************************************************************
 #include <pic.h>
 #include <string.h>
-
 
 #pragma config FOSC=INTOSC
 #pragma config WDTE=OFF
@@ -37,15 +37,23 @@
 #define EYE_ID                 PORTAbits.RA4    // input, eye identifier, left or right
 #define EYE_RX                 PORTCbits.RC5    // input, eye rx
 
-
 #define TEST                   LATAbits.LATA5   // output, debug
 
-static char symbol[6] = { 0x1e, 0x21, 0x27, 0x27, 0x21, 0x1e };
+static char visibleSymbol[6] = { 0x1e, 0x21, 0x27, 0x27, 0x21, 0x1e };
+static char tempRev[6];
 
-const char image1[6] = { 0x1e, 0x21, 0x2d, 0x2d, 0x21, 0x1e };
-const char image2[6] = { 0x1e, 0x21, 0x39, 0x39, 0x21, 0x1e };
-const char image3[6] = { 0x1e, 0x21, 0x27, 0x27, 0x21, 0x1e };
-const char image4[6] = { 0x1e, 0x21, 0x00, 0x00, 0x21, 0x1e };
+const char lookCentre[6] = { 0x1e, 0x21, 0x2d, 0x2d, 0x21, 0x1e };
+const char lookLeft[6] = { 0x1e, 0x21, 0x27, 0x27, 0x21, 0x1e };
+const char lookRight[6] = { 0x1e, 0x21, 0x39, 0x39, 0x21, 0x1e };
+const char lookNowhere[6] = { 0x1e, 0x21, 0x21, 0x21, 0x21, 0x1e };
+const char lookUp[6] = { 0x1e, 0x2d, 0x2d, 0x21, 0x21, 0x1e };
+const char lookDown[6] = { 0x1e, 0x21, 0x21, 0x2d, 0x2d, 0x1e };
+const char lookAngryUP[6] = { 0x00, 0x3c, 0x26, 0x27, 0x21, 0x1e };
+const char lookAngryDOWN[6] = { 0x00, 0x3c, 0x22, 0x27, 0x27, 0x1e };
+const char lookSad[6] = { 0x00, 0xF, 0x17, 0x27, 0x21, 0x1e };
+const char lookBored[6] = { 0x0, 0x1e, 0x3f, 0x2d, 0x21, 0x1e };
+const char lookSquint[6] = { 0x0, 0x1e, 0xd2, 0x2d, 0x1e, 0x0 };
+const char lookThoughtful[6] = { 0x0, 0x1e, 0x21, 0x2d, 0x2d, 0x1e };
 
 unsigned long  fiftymillisecs = 0 ; // IRQ Internal 50 millisecond counter.
 unsigned long  swtime         = 0 ;
@@ -55,41 +63,92 @@ unsigned char  txSlot   = 0;
 unsigned char  txEvent  = 0;
 unsigned char  tick     = 0;
 unsigned char  RxCmd    = 0;    // UART received character/command
+int orientation = 1;
 
+/*============================================================================*/
+unsigned char Reverse_bits(unsigned char num) 
+{
+    int i=5; //size of unsigned char -1, on most machine is 8bits
+    unsigned char j=0;
+    unsigned char temp=0;
 
+    while(i>=0){
+    temp |= ((num>>j)&1)<< i;
+    i--;
+    j++;
+    }
+    return(temp); 
+
+}
+
+/*============================================================================*/
 void interrupt OnlyOne_ISR(void)    // There is only one interrupt on this CPU
 {
-   /*============================================================================*/
    // Timer IRQ
    if (TMR1IE && TMR1IF)
       {
       TMR1   = 0xF858 ; //FG 0xF858
       TMR1IF = 0;
-		tick   = 1;
+      tick   = 1;
       timercnt++;
       }
 
-   /*============================================================================*/
-   if (TMR0IE && TMR0IF)          // check if Timer0 interrupt has occurred
+   if (TMR0IE && TMR0IF)          // check if Timer0 interrupt has occured
       {
-		TMR0IF = 0;
-		TMR0   = 65;
+      TMR0IF = 0;
+      TMR0   = 65;
       }
 
-   /*============================================================================*/
    if (RCIE && RCIF)          // UART RX interrupt has occurred
       {
-		RCIF  = 0;
-		RxCmd = RCREG;
+      RCIF  = 0;
+      RxCmd = RCREG;
         TEST   = ~TEST;
+       
+      switch (RxCmd) {
+            default :   {for (int i = 0; i < 6; i++)
+                        {
+                           visibleSymbol[i] = lookCentre[i];
+                        }
+                        break; }
+             case 1 :   {for (int i = 0; i < 6; i++)
+                        {
+                           visibleSymbol[i] = lookCentre[i];
+                        }
+                        break; }
+             case 2 :   {for (int i = 0; i < 6; i++)
+                        {
+                           visibleSymbol[i] = lookUp[i];
+                        }
+                        break; }
+             case 3 :   {for (int i = 0; i < 6; i++)
+                        {
+                           visibleSymbol[i] = lookDown[i];
+                        }
+                        break; }
+             case 4 :   {for (int i = 0; i < 6; i++)
+                        {
+                           visibleSymbol[i] = lookLeft[i];
+                        }
+                        break; }
+             case 5 :   {for (int i = 0; i < 6; i++)
+                        {
+                           visibleSymbol[i] = lookRight[i];
+                        }
+                        break; }
+         }
+          
+         if(orientation == 1){
+            for (int i = 0; i < 6; i++){
+                tempRev[i] = visibleSymbol[5-i];
+            }
+            for (int i = 0; i < 6; i++)
+            {
+               visibleSymbol[i] = Reverse_bits(tempRev[i]);
+            }
+         }
       }
-   /*============================================================================*/
-
 }
-/*============================================================================*/
-
-
-
 
 /*============================================================================*/
 void delayus(unsigned int us)
@@ -112,6 +171,26 @@ unsigned long get_milliseconds( )
 unsigned long delta_milliseconds ( unsigned long old_value )
 {
    return get_milliseconds ( ) - old_value ;
+}
+
+//UART transmit function, parameter Ch is the character to send
+void sio_putch(char ch)
+{
+   TXREG = ch;
+   while (TXSTAbits.TRMT == 0);   // wait for buffer to empty
+}
+
+/*============================================================================*/
+void sio_putstring(char *str)
+{
+   int i, len;
+
+   len =  strlen ( (char *) str ) ;
+   for ( i=0; i<len; i++ )
+      {
+      sio_putch( str[i] );
+      //delayus(100);
+      }
 }
 
 /*============================================================================*/
@@ -143,8 +222,8 @@ void setup_cpu ( void )
    TRISCbits.TRISC7 = 0;   // 
 
    // Initials Timer 0 settings
-   OPTION_REGbits.PS     = 0 ;	// pre-scaler is assigned to Timer0
-   OPTION_REGbits.PSA    = 0 ;	// pre-scaler is set to 2
+   OPTION_REGbits.PS     = 0 ;   // pre-scaler is assigned to Timer0
+   OPTION_REGbits.PSA    = 0 ;   // pre-scaler is set to 2
    OPTION_REGbits.TMR0CS = 0 ;
    INTCONbits.TMR0IE     = 0 ;   // Disable Timer0 interrupt
 
@@ -181,13 +260,31 @@ void setup_cpu ( void )
 
 }
 
-
+/*============================================================================*/
+void columnSwitch(int colInd, int rowInd){
+    switch (colInd)
+            {
+            case 0: if ( (visibleSymbol[rowInd] & 0x20) > 0) COL1 = 1; else COL1 = 0;
+               break;
+            case 1: if ( (visibleSymbol[rowInd] & 0x10) > 0) COL2 = 1; else COL2 = 0;
+               break;
+            case 2: if ( (visibleSymbol[rowInd] & 0x08) > 0) COL3 = 1; else COL3 = 0;
+               break;
+            case 3: if ( (visibleSymbol[rowInd] & 0x04) > 0) COL4 = 1; else COL4 = 0;
+               break;
+            case 4: if ( (visibleSymbol[rowInd] & 0x02) > 0) COL5 = 1; else COL5 = 0;
+               break;
+            case 5: if ( (visibleSymbol[rowInd] & 0x01) > 0) COL6 = 1; else COL6 = 0;
+               break;
+            }
+}
 
 /*============================================================================*/
 int main ( )
 {
    unsigned long idle_timer      = 0 ;
    unsigned char ridx, cidx, i = 0;
+   
 
    setup_cpu();
    ridx = 0;
@@ -197,69 +294,28 @@ int main ( )
       {
       CLRWDT();
 
-		if (tick == 1)
-		{
-			tick = 0;
-         if (cidx == 5)       // column index
-         {
-
-            //TEST   = ~TEST;
-
-            cidx = 0;
-            if (ridx == 5)    // row index
-            {
-               ridx = 0;
-            }
-            else
-            {
-               ridx++;
-            }
-         }
-         else
-         {
-            cidx++;
-         }
-
-         if (timercnt == 200)
-         {
-            timercnt = 0;
-            font_selection++;
-            if (font_selection == 4)
-            {
-               font_selection = 0;
-            }
-
-            switch (font_selection)
-            {
-            case 0: 
-               for (i = 0; i < 6; i++)
-               {
-                  symbol[i] = image2[i];
-               }
-               break;
-            case 1: 
-               for (i = 0; i < 6; i++)
-               {
-                  symbol[i] = image1[i];
-               }
-               break;
-            case 2: 
-               for (i = 0; i < 6; i++)
-               {
-                  symbol[i] = image3[i];
-               }
-               break;
-            case 3: 
-               for (i = 0; i < 6; i++)
-               {
-                  symbol[i] = image1[i];
-               }
-               break;
-            }
-         }
-
-
-
+      if (tick == 1)
+      {
+         tick = 0;
+         
+     
+        if (cidx == 5)       // column index
+        {  TEST   = ~TEST;
+           cidx = 0;
+           if (ridx == 5)    // row index
+           {
+              ridx = 0;
+           }
+           else
+           {
+              ridx++;
+           }
+        }
+        else
+        {
+           cidx++;
+        }        
+         
          switch (ridx)
          {
          default : 
@@ -278,48 +334,20 @@ int main ( )
             ROW4 = 1; COL4 = 0; 
             ROW5 = 1; COL5 = 0; 
             ROW6 = 1; COL6 = 0; 
-            
-            switch (cidx)
-            {
-            case 0: if ( (symbol[0] & 0x20) > 0) COL1 = 1; else COL1 = 0;
-               break;
-            case 1: if ( (symbol[0] & 0x10) > 0) COL2 = 1; else COL2 = 0;
-               break;
-            case 2: if ( (symbol[0] & 0x08) > 0) COL3 = 1; else COL3 = 0;
-               break;
-            case 3: if ( (symbol[0] & 0x04) > 0) COL4 = 1; else COL4 = 0;
-               break;
-            case 4: if ( (symbol[0] & 0x02) > 0) COL5 = 1; else COL5 = 0;
-               break;
-            case 5: if ( (symbol[0] & 0x01) > 0) COL6 = 1; else COL6 = 0;
-               break;
-            }
-				break;
+           
+            columnSwitch(cidx, ridx);
+            break;
 
-		   case 1 :
+         case 1 :
             ROW1 = 1; COL1 = 0;
             ROW2 = 0; COL2 = 0;
             ROW3 = 1; COL3 = 0;
             ROW4 = 1; COL4 = 0;
             ROW5 = 1; COL5 = 0;
             ROW6 = 1; COL6 = 0;
-            switch (cidx)
-            {
-            case 0: if ( (symbol[1] & 0x20) > 0) COL1 = 1; else COL1 = 0;
-               break;
-            case 1: if ( (symbol[1] & 0x10) > 0) COL2 = 1; else COL2 = 0;
-               break;
-            case 2: if ( (symbol[1] & 0x08) > 0) COL3 = 1; else COL3 = 0;
-               break;
-            case 3: if ( (symbol[1] & 0x04) > 0) COL4 = 1; else COL4 = 0;
-               break;
-            case 4: if ( (symbol[1] & 0x02) > 0) COL5 = 1; else COL5 = 0;
-               break;
-            case 5: if ( (symbol[1] & 0x01) > 0) COL6 = 1; else COL6 = 0;
-               break;
-            }
-
-				break;
+            
+            columnSwitch(cidx, ridx);
+            break;
 
          case 2 :
             ROW1 = 1; COL1 = 0;
@@ -329,23 +357,8 @@ int main ( )
             ROW5 = 1; COL5 = 0;
             ROW6 = 1; COL6 = 0;
 
-            switch (cidx)
-            {
-            case 0: if ( (symbol[2] & 0x20) > 0) COL1 = 1; else COL1 = 0;
-               break;
-            case 1: if ( (symbol[2] & 0x10) > 0) COL2 = 1; else COL2 = 0;
-               break;
-            case 2: if ( (symbol[2] & 0x08) > 0) COL3 = 1; else COL3 = 0;
-               break;
-            case 3: if ( (symbol[2] & 0x04) > 0) COL4 = 1; else COL4 = 0;
-               break;
-            case 4: if ( (symbol[2] & 0x02) > 0) COL5 = 1; else COL5 = 0;
-               break;
-            case 5: if ( (symbol[2] & 0x01) > 0) COL6 = 1; else COL6 = 0;
-               break;
-            }
-
-				break;
+            columnSwitch(cidx, ridx);
+            break;
 
          case 3 :
             ROW1 = 1; COL1 = 0;
@@ -355,22 +368,7 @@ int main ( )
             ROW5 = 1; COL5 = 0;
             ROW6 = 1; COL6 = 0;
 
-            switch (cidx)
-            {
-            case 0: if ( (symbol[3] & 0x20) > 0) COL1 = 1; else COL1 = 0;
-               break;
-            case 1: if ( (symbol[3] & 0x10) > 0) COL2 = 1; else COL2 = 0;
-               break;
-            case 2: if ( (symbol[3] & 0x08) > 0) COL3 = 1; else COL3 = 0;
-               break;
-            case 3: if ( (symbol[3] & 0x04) > 0) COL4 = 1; else COL4 = 0;
-               break;
-            case 4: if ( (symbol[3] & 0x02) > 0) COL5 = 1; else COL5 = 0;
-               break;
-            case 5: if ( (symbol[3] & 0x01) > 0) COL6 = 1; else COL6 = 0;
-               break;
-            }
-
+            columnSwitch(cidx, ridx);
             break;
 
          case 4 :
@@ -381,22 +379,7 @@ int main ( )
             ROW5 = 0; COL5 = 0;
             ROW6 = 1; COL6 = 0;
 
-            switch (cidx)
-            {
-            case 0: if ( (symbol[4] & 0x20) > 0) COL1 = 1; else COL1 = 0;
-               break;
-            case 1: if ( (symbol[4] & 0x10) > 0) COL2 = 1; else COL2 = 0;
-               break;
-            case 2: if ( (symbol[4] & 0x08) > 0) COL3 = 1; else COL3 = 0;
-               break;
-            case 3: if ( (symbol[4] & 0x04) > 0) COL4 = 1; else COL4 = 0;
-               break;
-            case 4: if ( (symbol[4] & 0x02) > 0) COL5 = 1; else COL5 = 0;
-               break;
-            case 5: if ( (symbol[4] & 0x01) > 0) COL6 = 1; else COL6 = 0;
-               break;
-            }
-
+            columnSwitch(cidx, ridx);
             break;
 
          case 5 :
@@ -407,81 +390,13 @@ int main ( )
             ROW5 = 1; COL5 = 0;
             ROW6 = 0; COL6 = 0;
 
-            switch (cidx)
-            {
-            case 0: if ( (symbol[5] & 0x20) > 0) COL1 = 1; else COL1 = 0;
-               break;
-            case 1: if ( (symbol[5] & 0x10) > 0) COL2 = 1; else COL2 = 0;
-               break;
-            case 2: if ( (symbol[5] & 0x08) > 0) COL3 = 1; else COL3 = 0;
-               break;
-            case 3: if ( (symbol[5] & 0x04) > 0) COL4 = 1; else COL4 = 0;
-               break;
-            case 4: if ( (symbol[5] & 0x02) > 0) COL5 = 1; else COL5 = 0;
-               break;
-            case 5: if ( (symbol[5] & 0x01) > 0) COL6 = 1; else COL6 = 0;
-               break;
-            }
-
+            columnSwitch(cidx, ridx);
             break;
 
          }  // switch
 
-
-		}	   // if tick
-
+      }     // if tick
 
       }
    return 0;
 }
-
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-			fiftymillisecs++;
-		    txSlot++;
-			tick = 0;
-EYE1 = ~EYE1;
-
-
-
-
-			if (txEvent == 1)
-			{
-				txEvent = 0;
-				if (txSlot == 0)
-				{
-				sio_putstring (messageSync);
-				}
-				else
-				{
-				sio_putstring (messageID);
-				}
-			}
-
-		}
-
-
-
-
-      CLRWDT();
-
-      }
-   return 0;
-}
-
-
-
-
-*/
-
